1e33909d94d5511f965dffc89ecc7bd95c278690
tellyworth <alex@automattic.com>
2025-12-19 14:02:33 +1100

    Store snapshots in a separate dir

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,8 @@ const LOCAL_WORKERS = process.env.PW_WORKERS
 
 export default defineConfig({
   testDir: './tests',
+  // Store all screenshots in a central test-snapshots directory
+  snapshotPathTemplate: 'test-snapshots/{arg}{ext}',
   fullyParallel: true, // Run tests in parallel with multiple workers
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
